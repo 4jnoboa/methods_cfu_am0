378c30113606c926e8f63cafc4c481704e7ea618
jose noboa <j.noboa@outlook.com>
2024-01-22 20:45:00 -0500

    updated hw

--- jose_hw.rb
+++ jose_hw.rb
@@ -1,27 +1,31 @@
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
-# Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
+# Hint: You will only write one check_stock method that checks the quantity and 
+#then prints the corresponding statement.
 
-#check_stock(4, "Coffee");
+def check_stock(stock , item)
+    if stock == 0
+        puts "#{item} is OUT of stock!"
+    elsif stock >= 1 && stock <= 3
+        puts "#{item} is running LOW"
+    elsif stock >= 4
+        puts "#{item} is stocked"
+    end
+end
+
+check_stock(25, "Coffee");
 # => "Coffee is stocked"
+check_stock(2, "grapes");
 
-#check_stock(3, "Tortillas");
+check_stock(3, "Tortillas");
 # => "Tortillas - running LOW"
 
-#check_stock(0, "Cheese");
+check_stock(0, "Cheese");
 # => "Cheese - OUT of stock!"
 
-# check_stock(1, "Salsa");
+check_stock(1, "Salsa");
 # => "Salsa - running LOW"
 
-stock = 1
-inventory = ["coffee", "cheese", "tortillas", "salsa"]
-if stock == 0
-    puts inventory[1] + " - OUT of stock!"
-elsif stock == 1
-    puts inventory[3] + " running LOW"
-elsif stock == 3
-    puts inventory[2] + " running LOW"
-else stock == 4
-    puts inventory[0] + " is stocked"
-end
+
+
+
 
